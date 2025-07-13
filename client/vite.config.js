aa95f789527ff4084adaaca5291db22455135d91
sachin <kinguniyal1311@gamail.com>
2025-07-13 21:55:50 +0530

    index.html,vite,config,vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-// C:\Users\Sachin\Desktop\sensei\client\vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-});
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures build output goes to 'dist'
+  }
+})
